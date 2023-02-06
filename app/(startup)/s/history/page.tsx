ce62cde51c25d08146a93159b27777722bbe1b3d
fernaldifz <fernaldifz@gmail.com>
2023-02-06 23:09:11 +0700

    fix history

--- app/(startup)/s/history/page.tsx
+++ app/(startup)/s/history/page.tsx
@@ -1,6 +1,7 @@
 'use client';
 import React, { useEffect, useState } from 'react';
 import Image from 'next/image';
+import Link from 'next/link';
 
 //assets imports
 import LeftArrow from '@/public/icons/left-arrow-icon.svg';
@@ -27,7 +28,7 @@ const GrantPointHistory: React.FC = () => {
             status: "sent",
             point: data.point,
             date: moment(data.createdAt).format('LLL'),
-            name: data.from.Name,
+            name: data.to.Name,
           };
         });
 
@@ -43,17 +44,17 @@ const GrantPointHistory: React.FC = () => {
     <>
       <div className="bg-white">
         <div className="flex">
-          <div className="mt-5 ml-4">
-            <Image src={LeftArrow} alt="Left Arrow" />
-          </div>
+          <Link className="mt-5 ml-4" href="/s/dashboard">
+            <Image src={LeftArrow} alt="Left Arrow" className='hover:cursor-pointer' />
+          </Link>
           <div className="ml-3 mt-5">
-            <h6>REDEMPTION</h6>
+            <h6>GRANT POINT</h6>
             <h6>HISTORY</h6>
           </div>{' '}
         </div>
         <div className="mt-5"></div>
-        {listData.map((data) => (
-          <div className="divide-y divide-slate-700">
+        {listData.map((data, id) => (
+          < div className="divide-y divide-slate-700" key={id}>
             <ListHistoryPoint
               status={data.status}
               name={data.name}
