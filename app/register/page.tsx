092cf9f44b8535b0120b331e69cc92876c8f071d
Rafi Raihansyah <13519154@std.stei.itb.ac.id>
2023-02-24 10:50:53 +0700

    fix: image & login register

--- app/register/page.tsx
+++ app/register/page.tsx
@@ -1,34 +1,34 @@
 'use client';
 
 import Register from '@/components/Register/Register';
-import { useAuth } from '@/context/AuthContext';
-import { useRouter } from 'next/navigation';
+// import { useAuth } from '@/context/AuthContext';
+// import { useRouter } from 'next/navigation';
 import React from 'react';
 
 interface RegisterPageProps {}
 
 const RegisterPage: React.FC<RegisterPageProps> = () => {
-  const router = useRouter();
-  const { isAuthenticated, getUser } = useAuth();
-  const user = getUser();
+  // const router = useRouter();
+  // const { isAuthenticated, getUser } = useAuth();
+  // const user = getUser();
 
-  if (isAuthenticated()) {
-    switch (user.role) {
-      case 'user':
-        router.push('/u/dashboard');
-        break;
-      case 'admin':
-        router.push('/a/dashboard');
-        break;
-      case 'startup':
-        router.push('/s/dashboard');
-        break;
+  // if (isAuthenticated()) {
+  //   switch (user.role) {
+  //     case 'user':
+  //       router.push('/u/dashboard');
+  //       break;
+  //     case 'admin':
+  //       router.push('/a/dashboard');
+  //       break;
+  //     case 'startup':
+  //       router.push('/s/dashboard');
+  //       break;
 
-      default:
-        router.push('/login');
-        break;
-    }
-  }
+  //     default:
+  //       router.push('/login');
+  //       break;
+  //   }
+  // }
 
   return (
     <>
